118d70461002efefa86c867c3f348d985678fb26
Neha Arora <62641795+NehaArora9@users.noreply.github.com>
2025-02-15 02:14:32 +0530

    Update ErrorViewModel.cs

--- app/SampleWebApp/Models/ErrorViewModel.cs
+++ app/SampleWebApp/Models/ErrorViewModel.cs
@@ -6,6 +6,6 @@ namespace SampleWebApp.Models
     {
         public string RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId)
     }
 }
